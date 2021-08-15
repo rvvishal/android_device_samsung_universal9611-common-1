9d2125d491285f6f11c8da63281a44ab53fd3d09
rvvishal <vidhalrajbhar6000@gmail.com>
2021-08-15 12:01:59 +0000

    universal9611 : Add stock init and recovery files from stock rom

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -17,17 +17,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.recovery.exynos9611.rc
-LOCAL_SRC_FILES := etc/init.recovery.exynos9611.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_OUT)/root
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := ueventd.recovery.exynos9611.rc
-LOCAL_SRC_FILES := etc/ueventd.rc
-LOCAL_MODULE_STEM := ueventd
-LOCAL_MODULE_SUFFIX := rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_OUT)/root
+LOCAL_MODULE            := init.recovery.exynos9611.rc
+LOCAL_MODULE_TAGS       := optional
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.recovery.exynos9611.rc
+LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
